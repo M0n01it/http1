a52fb12e84172c0e8bdf8e97f94bfcd28471cdb7
Nikolay Golovin <iimonolitii@mail.ru>
2025-01-12 20:57:19 +0300

    Add query parsing

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,15 +1,18 @@
 package ru.netology;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
-  public static void main(String[] args) throws InterruptedException {
-    List<String> validPaths = List.of("/index.html", "/about.html", "/contact.html");
-    Server server = new Server(8080, validPaths);
-    new Thread(server::start).start();
+  public Main() {
+  }
 
-    // Остановка сервера через 60 секунд
-    Thread.sleep(60000);
+  public static void main(String[] args) throws InterruptedException {
+    List<String> validPaths = List.of("/index.html", "/about.html", "/contact.html", "/messages");
+    Server server = new Server(8080, validPaths);
+    Objects.requireNonNull(server);
+    (new Thread(server::start)).start();
+    Thread.sleep(60000L);
     server.stop();
   }
 }
